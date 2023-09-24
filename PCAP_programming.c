c414fbc111762ee391a683bf8dd7cc9a4ac49b36
yaejida <108354984+songyaeji@users.noreply.github.com>
2023-09-24 22:30:02 +0900

    Update PCAP_programming.c
    raw data -> message

--- PCAP_programming.c
+++ PCAP_programming.c
@@ -9,48 +9,43 @@ void packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u
     struct ether_header *eth_header;
     struct ip *ip_header;
     struct tcphdr *tcp_header;
-    
+    char *data;
+
     eth_header = (struct ether_header *)packet;
     if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
-        // Skip non-IP packets
+        // 이더넷 프레임 타입이 IP가 아닌 경우 패스
         return;
     }
 
     ip_header = (struct ip *)(packet + ETHER_HDR_LEN);
     if (ip_header->ip_p != IPPROTO_TCP) {
-        // Skip non-TCP packets
+        // IP 패킷의 프로토콜이 TCP가 아닌 경우 패스
         return;
     }
 
     tcp_header = (struct tcphdr *)(packet + ETHER_HDR_LEN + (ip_header->ip_hl << 2));
+    data = (char *)(packet + ETHER_HDR_LEN + (ip_header->ip_hl << 2) + (tcp_header->th_off << 2));
 
-    // Extract and print Ethernet source and destination MAC addresses
-    printf("Ethernet Source MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+    // 이더넷 소스 및 대상 MAC 주소 출력
+    printf("Ethernet 소스 MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
            eth_header->ether_shost[0], eth_header->ether_shost[1],
            eth_header->ether_shost[2], eth_header->ether_shost[3],
            eth_header->ether_shost[4], eth_header->ether_shost[5]);
-    printf("Ethernet Destination MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+    printf("Ethernet 대상 MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
            eth_header->ether_dhost[0], eth_header->ether_dhost[1],
            eth_header->ether_dhost[2], eth_header->ether_dhost[3],
            eth_header->ether_dhost[4], eth_header->ether_dhost[5]);
 
-    // Extract and print source and destination IP addresses
+    // 소스 및 대상 IP 주소 출력
     printf("Source IP: %s\n", inet_ntoa(ip_header->ip_src));
     printf("Destination IP: %s\n", inet_ntoa(ip_header->ip_dst));
 
-    // Extract and print source and destination ports
+    // 소스 및 대상 포트 출력
     printf("Source Port: %d\n", ntohs(tcp_header->th_sport));
     printf("Destination Port: %d\n", ntohs(tcp_header->th_dport));
 
-    // Extract and print message (data)
-    int data_length = pkthdr->len - (ETHER_HDR_LEN + (ip_header->ip_hl << 2) + (tcp_header->th_off << 2));
-    if (data_length > 0) {
-        printf("Message Data: ");
-        for (int i = 0; i < data_length; i++) {
-            printf("%02X ", packet[ETHER_HDR_LEN + (ip_header->ip_hl << 2) + (tcp_header->th_off << 2) + i]);
-        }
-        printf("\n");
-    }
+    // 메시지 출력
+    printf("Message: %s\n", data);
 
     printf("\n");
 }
@@ -59,17 +54,17 @@ int main() {
     pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    // Open live pcap session on the desired network interface (change "enp0s3" to your interface name)
+    // 원하는 네트워크 인터페이스에 대한 라이브 pcap 세션 열기 (인터페이스 이름을 변경하세요)
     handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-        fprintf(stderr, "Error opening interface: %s\n", errbuf);
+        fprintf(stderr, "인터페이스 열기 오류: %s\n", errbuf);
         return 1;
     }
 
-    // Start capturing packets
+    // 패킷 캡처 시작
     pcap_loop(handle, 0, packet_handler, NULL);
 
-    // Close the pcap session when done
+    // 사용이 끝난 pcap 세션 닫기
     pcap_close(handle);
 
     return 0;
